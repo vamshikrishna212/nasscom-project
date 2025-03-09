a448127b55404501aa97b54c78f465d54d1363ba
vamshikrishna212 <k.vamshikrishna1794058@gmail.com>
2025-03-09 15:20:10 +0530

    Refined code

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from few_shot import FewShotPosts
 from post_generator import generate_post
 
 
@@ -15,8 +14,6 @@ def main():
     # Create three columns for the dropdowns
     col1, col2, col3 = st.columns(3)
 
-    fs = FewShotPosts()
-    #tags = fs.get_tags()
     tags = ["Formal", "Casual", "Friendly", "Professional", "Persuasive", "Apologetic", "Appreciative", "Introduction", "Follow-up", "Thank You", "Apology", "Complaint", "Invitation", "Job Application", "Networking", "Reminder", "Announcement", "Feedback Request", "Sales Pitch", "Very Formal", "Semi-Formal", "Informal", "Colleague", "Boss", "Client", "Customer", "Friend", "Professor", "Recruiter", "Vendor", "Bullet Points", "Paragraph-Based", "Hybrid", "Low", "Moderate", "High", "Personalized", "Generic", "Template-Based", "No Response Needed", "Response Expected", "Follow-Up Required"]
     with col1:
         # Dropdown for Topic (Tags)
@@ -37,10 +34,6 @@ def main():
     # Generate Button
     if st.button("Generate"):
         post = generate_post(selected_length, selected_language, selected_tag,scenario,sender,receiver)
-#         post = '''
-#         Dear Manager,\n\n\tI am writing to inform you that, unfortunately, I will be unable to come to work for the next few days.\n\n\tI am currently not feeling well and my health is not in the best condition.\n\n\tI have been diagnosed with an illness that requires me to take some time off to recover.\n\n\tI apologize for any inconvenience this may cause and will make sure to catch up on any missed work as soon as possible.\n\n\tI will keep you updated on my status and provide a doctor\'s note if needed.\n\n\tThe expected dates of my leave are from [start date] to [end date].\n\n\tI will be available by email if anything urgent comes up while I am away.\n\n\tPlease let me know if there are any pressing matters that need my attention before my leave.\n\n\tI appreciate your understanding and support during this time.\n\n\tIf there is anything I can do to mitigate the impact of my absence, please let me know.\n\n\tI am committed to my responsibilities and will ensure a smooth transition of tasks.\n\n\tI will respond to all emails and messages as soon as I am feeling better.\n\n\tThank you for your consideration and I look forward to returning to work as soon as possible.\n\n\tPlease do not hesitate to contact me if you have any questions or concerns.\n\nSincerely,\n\n[Your Name]\n\nBest regards, \n\n[Your Name]
-# Dear Manager,\n\n\tI am writing to inform you that, unfortunately, I will be unable to come to work for the next few days.\n\n\tI am currently not feeling well and my health is not in the best condition.\n\n\tI have been diagnosed with an illness that requires me to take some time off to recover.\n\n\tI apologize for any inconvenience this may cause and will make sure to catch up on any missed work as soon as possible.\n\n\tI will keep you updated on my status and provide a doctor\'s note if needed.\n\n\tThe expected dates of my leave are from [start date] to [end date].\n\n\tI will be available by email if anything urgent comes up while I am away.\n\n\tPlease let me know if there are any pressing matters that need my attention before my leave.\n\n\tI appreciate your understanding and support during this time.\n\n\tIf there is anything I can do to mitigate the impact of my absence, please let me know.\n\n\tI am committed to my responsibilities and will ensure a smooth transition of tasks.\n\n\tI will respond to all emails and messages as soon as I am feeling better.\n\n\tThank you for your consideration and I look forward to returning to work as soon as possible.\n\n\tPlease do not hesitate to contact me if you have any questions or concerns.\n\nSincerely,\n\n[Your Name]\n\nBest regards, \n\n[Your Name]
-# '''
         cssString = '''
             @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap');
             .email-template {
--- post_generator.py
+++ post_generator.py
@@ -1,8 +1,4 @@
 from llm_helper import llm
-from few_shot import FewShotPosts
-
-few_shot = FewShotPosts()
-
 
 def get_length_str(length):
     if length == "Short":
@@ -16,48 +12,44 @@ def get_length_str(length):
 def generate_post(length, language, tag,scenario,sender,receiver):
     prompt = get_prompt(length, language, tag,scenario,sender,receiver)
     response = llm.invoke(prompt)
-    print(response.content)
+    #print(response.content)
     return response.content
 
 
 def get_prompt(length, language, tag,scenario,sender,receiver):
     length_str = get_length_str(length)
 
-    
-    # prompt = prompt.format(post_topic=tag, post_length=length_str, post_language=language)
-
-    #examples = few_shot.get_filtered_posts(length, language, tag)
     prompt = '''
         Generate an email in HTML format for display on a website. Follow these rules: NO PREAMBLE  
-1. **Type**: {tag}
-2. **Length**:{length_str}  
-3. **Language**:  {language}
-4. **Scenario**: {scenario}
-5. **Sender's Name** : {sender}
-5. **Receiver's Name/Designation** : {receiver}
+        1. **Type**: {tag}
+        2. **Length**:{length_str}  
+        3. **Language**:  {language}
+        4. **Scenario**: {scenario}
+        5. **Sender's Name** : {sender}
+        5. **Receiver's Name/Designation** : {receiver}
 
-**Requirements**:  
-- Wrap the entire email in a `<div class='email-template'>` tag.  
-- Use `<p class="email-para">` tags for paragraphs and `<br>` for line breaks .
-- Each paragraph must be  minimum of 4-5 lines  
-- Highlight placeholders like [Name] with `<span class='placeholder'>[Name]</span>`.  
-- Include a subject line inside `<p class='subject'>`.  
-- Preserve indentation and line breaks using proper HTML (no Markdown).  
+        **Requirements**:  
+        - Wrap the entire email in a `<div class='email-template'>` tag.  
+        - Use `<p class="email-para">` tags for paragraphs and `<br>` for line breaks .
+        - Each paragraph must be  minimum of 4-5 lines  
+        - Highlight placeholders like [Name] with `<span class='placeholder'>[Name]</span>`.  
+        - Include a subject line inside `<p class='subject'>`.  
+        - Preserve indentation and line breaks using proper HTML (no Markdown).  
 
 
-Return **only the HTML code**, no explanations.
+        Return **only the HTML code**, no explanations.
 
     '''.format(tag=tag, length_str=length_str, language=language, scenario=scenario,sender=sender,receiver=receiver)
-    examples = []
-    if len(examples) > 0:
-        prompt += "4) Use the writing style as per the following examples."
+    # examples = []
+    # if len(examples) > 0:
+    #     prompt += "4) Use the writing style as per the following examples."
 
-    for i, post in enumerate(examples):
-        post_text = post['text']
-        prompt += f'\n\n Example {i+1}: \n\n {post_text}'
+    # for i, post in enumerate(examples):
+    #     post_text = post['text']
+    #     prompt += f'\n\n Example {i+1}: \n\n {post_text}'
 
-        if i == 1: # Use max two samples
-            break
+    #     if i == 1: # Use max two samples
+    #         break
 
     return prompt
 
